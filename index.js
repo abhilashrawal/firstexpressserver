d6161638819376ff8c365e254609544b78d5af19
unknown <abhilashrawal091@gmail.com>
2023-01-27 12:10:16 +0530

    view

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+  res.render('index',{
+    title: "node class"
+  })
+})
+app.get('viewlist', (req,res) =>{
+    res.render('index',{
+        title
+    })
+})
+// app.use(path.join(__dirname, 'public'))
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
